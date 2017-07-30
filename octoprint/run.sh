d198e2d7a74f10c441992ef017a80d83cf80a401
Fredrik Baberg <fbaberg@kth.se>
2017-07-30 22:53:22 +0200

    Changed runfile.

--- octoprint/run.sh
+++ octoprint/run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sudo -u octoprint /home/octoprint/OctoPrint/venv/bin/octoprint serve --config /config/octoprint/config.yaml --basedir /config/octoprint
-# octoprint start
+octoprint start
