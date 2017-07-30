40e8f41159b22c49b933cee62b3e3a37e9b34ec7
Fredrik Baberg <fbaberg@kth.se>
2017-07-30 06:07:50 +0200

    Restart from within octoprint does not work, but other than that config seems to be OK. need to test on printer.

--- octoprint/run.sh
+++ octoprint/run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 ls -lah /data
-/home/octoprint/OctoPrint/venv/bin/octoprint serve
+sudo mkdir -p /config/octoprint
+sudo chown -R octoprint:octoprint /config/octoprint
+if [ ! -f /config/octoprint/config.yaml ]; then
+    cp /home/octoprint/.octoprint/config.yaml /config/octoprint/config.yaml
+fi
+/home/octoprint/OctoPrint/venv/bin/octoprint serve --config /config/octoprint/config.yaml --basedir /config/octoprint
