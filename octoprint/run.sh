b8221720304427cbe851d0107878d9509245b923
Fredrik Baberg <fbaberg@kth.se>
2017-08-01 23:00:19 +0200

    Testing to move OctoPrint to data folder.

--- octoprint/run.sh
+++ octoprint/run.sh
@@ -5,4 +5,11 @@ sudo chown -R octoprint:octoprint /config/octoprint
 if [ ! -f /config/octoprint/config.yaml ]; then
     cp /home/octoprint/.octoprint/config.yaml /config/octoprint/config.yaml
 fi
+if [ ! -f /data/Cura ]; then
+    cp -r /home/octoprint/Cura /data/
+fi
+if [ ! -f /data/OctoPrint ]; then
+    cp -r /home/octoprint/OctoPrint /data/
+fi
 sudo -u octoprint /home/octoprint/OctoPrint/venv/bin/octoprint serve --config /config/octoprint/config.yaml --basedir /config/octoprint
+sudo -u octoprint /data/OctoPrint/venv/bin/octoprint serve --config /config/octoprint/config.yaml --basedir /config/octoprint
