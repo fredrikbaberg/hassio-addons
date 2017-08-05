3a0d51090acb8fd661772ba689497a07c714d1f5
Fredrik Baberg <fbaberg@kth.se>
2017-08-05 12:03:03 +0200

    Should be ready to use, haven't tested ttyUSSB.

--- octoprint/octoprint.sh
+++ octoprint/octoprint.sh
@@ -18,35 +18,3 @@ setup() {
 
 setup
 /data/OctoPrint/venv/bin/octoprint daemon restart --config /config/octoprint/config.yaml --basedir /config/octoprint >> /home/octoprint/log_octo.txt
-# /data/OctoPrint/venv/bin/octoprint daemon restart --config /config/octoprint/config.yaml --basedir /config/octoprint > /home/octoprint/octo_out.txt
-# /data/OctoPrint/venv/bin/octoprint daemon stop
-# /data/OctoPrint/venv/bin/octoprint daemon restart --config /config/octoprint/config.yaml --basedir /config/octoprint
-
-# # Kill process, if running.
-# stop() {
-#     if [ -f /data/current_octoprint_pid ]; then
-#         kill -15 $(cat /data/current_octoprint_pid)
-#         sudo rm -f /data/current_octoprint_pid
-#     fi
-# }
-
-# # Run octoprint and store ID
-# start() {
-#     sudo -u octoprint /data/OctoPrint/venv/bin/octoprint serve --config /config/octoprint/config.yaml --basedir /config/octoprint & echo $! >> /data/current_octoprint_pid
-# }
-
-# if [ "$1" = "stop" ]; then
-#     echo "========== Stop octoprint =========="
-#     stop
-# else
-#     echo "========== (re)Start octoprint =========="
-#     setup
-#     echo "========== Folders created =========="
-#     stop
-#     echo "========== Previous process killed =========="
-#     sleep 2
-#     start
-#     echo "========== Started octoprint =========="
-# fi
-
-# /data/OctoPrint/venv/bin/octoprint daemon start --config /config/octoprint/config.yaml --basedir /config/octoprint
