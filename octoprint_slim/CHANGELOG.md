9d38fa1f94a649968291803002ebd327dab4592d
Fredrik Baberg <fbaberg@kth.se>
2019-05-16 12:51:51 +0200

    Corrections to slim version (fix network port, v.1.3.11) (#34)
    * Corrections to slim version (fix network port, upgrade OctoPrint to 1.3.11)

--- octoprint_slim/CHANGELOG.md
+++ octoprint_slim/CHANGELOG.md
@@ -1,10 +1,14 @@
 # Changelog
 
-[0.0.1]
-Initial Slim version (Octoprint without CURA, MJPEG and FFMPEG).
-
-[0.0.2]
-Build using TRAVIS.
+[0.0.4]
+- Correction of exposed ports.
+- Update to OctoPrint 1.3.11
 
 [0.0.3]
-Use UDP instead of TCP for port.
+- Use UDP instead of TCP for port.
+
+[0.0.2]
+- Build using TRAVIS.
+
+[0.0.1]
+- Initial Slim version (Octoprint without CURA, MJPEG and FFMPEG).
--- octoprint_slim/build_test.sh
+++ octoprint_slim/build_test.sh
@@ -2,5 +2,5 @@
 # Build a Docker image and test to launch it (replacing the entrypoint with /bin/sh). For testing.
 mkdir -p my_test_data && mkdir -p my_test_config
 sudo docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/octoprint_test .
-sudo docker run -it --rm -v $(pwd)/my_test_data:/data -v $(pwd)/my_test_config:/config -p 5000:80 --entrypoint /bin/sh local/octoprint_test
+sudo docker run -it --rm -v $(pwd)/my_test_data:/data -v $(pwd)/my_test_config:/config -p 5000:5000 --entrypoint /bin/sh local/octoprint_test
 # docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t octoprint -r https://github.com/fredrikbaberg/hassio-addons -b build_update
