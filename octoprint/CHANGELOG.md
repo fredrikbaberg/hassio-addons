009ccdc69043bfdf48a7185d9e646460bff9e650
Fredrik Baberg <fredrik.baberg@gmail.com>
2020-04-21 23:24:12 +0000

    Attempt to use Ingress with octoprint

--- octoprint/CHANGELOG.md
+++ octoprint/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+[0.2.7]
+- Works with HassOS
+- Bonjour enabled
+- Ready for camera support
+
 [0.2.6]
 - Corrections to work with newer version of Hass.IO.
 - Should allow other USB devices than ttyUSB0
--- octoprint/build_test.sh
+++ octoprint/build_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Build a Docker image and test to launch it (replacing the entrypoint with /bin/sh). For testing.
+export DEV_VIDEO=/dev/null
 mkdir -p my_test_data && mkdir -p my_test_config
-docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/octoprint_test .
-docker run -it --rm -v $(pwd)/my_test_data:/data -v $(pwd)/my_test_config:/config -p 5000 --entrypoint /bin/sh local/octoprint_test
+sudo docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/octoprint_test .
+sudo docker run -it --rm -v $(pwd)/my_test_data:/data -v $(pwd)/my_test_config:/config -p 5000:80 --device=$DEV_VIDEO:/dev/video0 --entrypoint /bin/sh local/octoprint_test
 # docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t octoprint -r https://github.com/fredrikbaberg/hassio-addons -b build_update
