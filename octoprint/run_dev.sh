0936db078c0134f57dd708d3e1727e53a1ff829a
Fredrik Baberg <fredrik.baberg@gmail.com>
2020-04-22 22:01:16 +0000

    Conditional adding lines to config

--- octoprint/run_dev.sh
+++ octoprint/run_dev.sh
@@ -6,18 +6,21 @@ set -e
 
 copy_data() {
     if [ ! -d /data/python ]; then
+        # bashio::log.info "Copy data"
         echo "Copy data"
         cp -R /root/python /data/
     fi
 }
 
-create_homeassistant_user() {
+create_ingress_user() {
+    # bashio::log.info "Create ingress user"
     echo "Create ingress user"
     octoprint --basedir /data --config /config/octoprint/config.yaml user add homeassistant --password octoprint --admin 2> /dev/null
 }
 
 create_config() {
     if [ ! -f /config/octoprint/config.yaml ]; then
+        # bashio::log.info "Create config"
         echo "Create config"
         mkdir -p /config/octoprint/
         cd /config/octoprint
@@ -27,14 +30,39 @@ create_config() {
         echo "  autologinLocal: true" >> config.yaml
         echo "  localNetworks:" >> config.yaml
         echo "    - 172.30.32.2" >> config.yaml
-        echo "    - 192.168.1.175" >> config.yaml
-        echo "server:" >> config.yaml
-        echo "  firstRun: false" >> config.yaml
-        echo "  commands:" >> config.yaml
-        echo "    serverRestartCommand: supervisorctl reload" >> config.yaml
         echo "devel:" >> config.yaml
         echo "  virtualPrinter:" >> config.yaml
         echo "    enabled: true" >> config.yaml
+        echo "plugins:" >> config.yaml
+        if [ "${INSTALL_CURA_LEGACY}" != "0" ]; then
+            echo "  cura:" >> config.yaml
+            echo "    cura_engine: /sbin/CuraEngine" >> config.yaml
+            echo "    debug_logging: false" >> config.yaml
+        fi
+        echo "  pluginmanager:" >> config.yaml
+        echo "    pip_force_user: true" >> config.yaml
+        echo "serial:" >> config.yaml
+        echo "  additionalPorts:" >> config.yaml
+        echo "  - /dev/tty*" >> config.yaml
+        echo "server:" >> config.yaml
+        echo "  commands:" >> config.yaml
+        echo "    serverRestartCommand: supervisorctl reload" >> config.yaml
+        if [ "${INSTALL_CAMERA_SUPPORT}" != "0" ]; then
+            echo "system:" >> config.yaml
+            echo "  actions:" >> config.yaml
+            echo "  - action: streamon" >> config.yaml
+            echo "    command: supervisorctl start mjpeg-streamer" >> config.yaml
+            echo "    confirm: false" >> config.yaml
+            echo "    name: Start webcam" >> config.yaml
+            echo "  - action: streamoff" >> config.yaml
+            echo "    command: supervisorctl stop mjpeg-streamer" >> config.yaml
+            echo "    confirm: false" >> config.yaml
+            echo "    name: Stop webcam" >> config.yaml
+            echo "webcam:" >> config.yaml
+            echo "  stream: /webcam/?action=stream" >> config.yaml
+            echo "  snapshot: http://127.0.0.1:8080/?action=snapshot" >> config.yaml
+            echo "  ffmpeg: /usr/bin/ffmpeg" >> config.yaml
+        fi
     fi
 }
 
@@ -65,8 +93,9 @@ set_ingress_entry() {
 copy_data
 create_config
 # create_logging_file # Don't modify logging.
-create_homeassistant_user # Ensure homeassistant user exist, should not modify existing users.
+create_ingress_user # Ensure Ingress user (homeassistant) exist. This should not modify existing users.
 set_ingress_entry
+# bashio::log.info "Launch"
 echo "Launch"
 /usr/bin/supervisord -c /etc/supervisord.conf
 tail -f /tmp/octoprint-stdout*
