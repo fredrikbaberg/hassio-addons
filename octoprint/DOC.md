60a90ddad0c96e68dea0266eae29cc9c2f49b296
Fredrik Baberg <fredrik.baberg@gmail.com>
2020-05-13 10:45:54 +0200

    Experimental with Python2 (#67)
    * Move configuration of experimental to separate folder
    * Add reset data option
    * Refactored Dockerfile
    * Skip restart and shutdown commands for now
    * Should not need pip force user now
    * Describe reset command
    * Update changelog
    * start on boot manual
    * Enable cache on HAProxy
    * Increase version number, store timelapse tmp in config

--- octoprint/DOC.md
+++ octoprint/DOC.md
@@ -1,9 +1,7 @@
 # Documentation
 
-All configuration should be done using OctoPrint WebUI.
-
 ## Access
 
-There is a default user created called `homeassistant`, this is for access through Ingress. At first launch you should change the password for this user.
+There is a default user created called `homeassistant`, this is for access through Ingress. The password is randomized at first launch.
 
-In case you want to be able to access OctoPrint without using Ingress, specify the port in the port section of the addon.
+In order to access OctoPrint without Ingress, specify a port in the port section of the addon.
--- octoprint/run.sh
+++ octoprint/run.sh
@@ -2,18 +2,26 @@
 # #!/bin/sh
 # set -e
 
+reset_data_if_requested(){
+    if bashio::config.true 'request_reset_data'; then
+        rm -rf /data/*
+        bashio::log.info "Data has been reset"
+    fi
+}
+
 copy_data() {
     if [ ! -d /data/python ]; then
-        bashio::log.info "Move data"
-        # echo "Move data"
-        mv /root/python /data/
+        bashio::log.info "Copy data to persistent location"
+        # echo "Copy data to persistent location"
+        cp -R /root/python /data/
     fi
 }
 
 create_ingress_user() {
     bashio::log.info "Create ingress user"
     # echo "Create ingress user"
-    octoprint --basedir /data --config /config/octoprint/config.yaml user add homeassistant --password octoprint --admin 2> /dev/null
+    new_password=`date +%s | sha256sum | base64 | head -c 32 ; echo`
+    octoprint --basedir /data/octoprint --config /config/octoprint/config.yaml user add homeassistant --password $new_password --admin # 2> /dev/null
 }
 
 create_config() {
@@ -21,7 +29,7 @@ create_config() {
         bashio::log.info "Create config"
         # echo "Create config"
         mkdir -p /config/octoprint/
-        cd /config/octoprint
+        cd /config/octoprint/
         touch config.yaml
         echo "accessControl:" >> config.yaml
         echo "  autologinAs: homeassistant" >> config.yaml
@@ -31,51 +39,40 @@ create_config() {
         echo "devel:" >> config.yaml
         echo "  virtualPrinter:" >> config.yaml
         echo "    enabled: true" >> config.yaml
+        echo "folder:" >> config.yaml
+        echo "  timelapse: /config/octoprint/timelapse" >> config.yaml
+        echo "  timelapse_tmp: /config/octoprint/timelapse/tmp" >> config.yaml
+        echo "  uploads: /config/octoprint/uploads" >> config.yaml
+        echo "  watched: /config/octoprint/watched" >> config.yaml
         echo "plugins:" >> config.yaml
         echo "  cura:" >> config.yaml
         echo "    cura_engine: /sbin/CuraEngine" >> config.yaml
         echo "    debug_logging: false" >> config.yaml
         echo "  pluginmanager:" >> config.yaml
-        echo "    pip_force_user: true" >> config.yaml
+        echo "    pip_force_user: false" >> config.yaml
         echo "serial:" >> config.yaml
         echo "  additionalPorts:" >> config.yaml
         echo "  - /dev/tty*" >> config.yaml
         echo "server:" >> config.yaml
         echo "  commands:" >> config.yaml
         echo "    serverRestartCommand: supervisorctl reload" >> config.yaml
+        # echo "    systemRestartCommand: bashio::addon.restart" >> config.yaml
+        # echo "    systemShutdownCommand: bashio::addon.stop" >> config.yaml
         echo "system:" >> config.yaml
         echo "  actions:" >> config.yaml
-        echo "  - action: streamon" >> config.yaml
-        echo "    command: supervisorctl start mjpeg-streamer" >> config.yaml
-        echo "    confirm: false" >> config.yaml
-        echo "    name: Start webcam" >> config.yaml
-        echo "  - action: streamoff" >> config.yaml
-        echo "    command: supervisorctl stop mjpeg-streamer" >> config.yaml
-        echo "    confirm: false" >> config.yaml
-        echo "    name: Stop webcam" >> config.yaml
+        # echo "  - action: streamon" >> config.yaml
+        # echo "    command: supervisorctl start mjpeg-streamer" >> config.yaml
+        # echo "    confirm: false" >> config.yaml
+        # echo "    name: Start webcam" >> config.yaml
+        # echo "  - action: streamoff" >> config.yaml
+        # echo "    command: supervisorctl stop mjpeg-streamer" >> config.yaml
+        # echo "    confirm: false" >> config.yaml
+        # echo "    name: Stop webcam" >> config.yaml
         echo "webcam:" >> config.yaml
-        echo "  stream: /webcam/?action=stream" >> config.yaml
-        echo "  snapshot: http://127.0.0.1:8080/?action=snapshot" >> config.yaml
+        # echo "  stream: /webcam/?action=stream" >> config.yaml
+        # echo "  snapshot: http://127.0.0.1:8080/?action=snapshot" >> config.yaml
         echo "  ffmpeg: /usr/bin/ffmpeg" >> config.yaml
     fi
-}
-
-create_logging_file() {
-    # Use this to clean up some of the logging.
-    cd /data
-    touch logging.yaml
-    echo "" > logging.yaml # Replace content.
-    echo "loggers:" >> logging.yaml
-    for level_warn in "octoprint" "octoprint.util.pip" "octoprint.plugins.pluginmanager" "octoprint.plugins.softwareupdate" "octoprint.plugins.discovery" "octoprint.plugins.octoprint.plugins.discovery" "octoprint.server.util.flask"
-    do
-        echo "  $level_warn:" >> logging.yaml
-        echo "    level: WARN" >> logging.yaml
-    done
-    for level_info in "octoprint.server.util.sockjs"
-    do
-        echo "  $level_info:" >> logging.yaml
-        echo "    level: INFO" >> logging.yaml
-    done
 }
 
 set_ingress_entry() {
@@ -84,9 +81,9 @@ set_ingress_entry() {
     # sed -e '/http-request set-header X-Script-Name/s/^/#/g' -i /etc/haproxy/haproxy.cfg
 }
 
+reset_data_if_requested
 copy_data
 create_config
-# create_logging_file # Don't modify logging.
 create_ingress_user # Ensure Ingress user (homeassistant) exist. This should not modify existing users.
 set_ingress_entry
 bashio::log.info "Launch"
