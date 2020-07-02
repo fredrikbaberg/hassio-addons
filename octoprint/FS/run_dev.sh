4653791616245495434b4824a44a6b541d3a536b
Fredrik Baberg <fredrik.baberg@gmail.com>
2020-07-02 21:16:40 +0200

    Move OctoPrint config to separate file

--- octoprint/FS/run_dev.sh
+++ octoprint/FS/run_dev.sh
@@ -37,61 +37,7 @@ create_config() {
         # bashio::log.info "Create config"
         echo "Create config"
         mkdir -p /config/octoprint
-        cd /config/octoprint
-        touch config.yaml
-        echo "accessControl:" >> config.yaml
-        echo "  autologinAs: homeassistant" >> config.yaml
-        echo "  autologinLocal: true" >> config.yaml
-        echo "  localNetworks:" >> config.yaml
-        echo "    - 172.30.32.2" >> config.yaml
-        echo "devel:" >> config.yaml
-        echo "  virtualPrinter:" >> config.yaml
-        echo "    enabled: true" >> config.yaml
-        echo "folder:" >> config.yaml
-        # Non-user-accessible folders
-        echo "  data: /data/octoprint/data" >> config.yaml
-        echo "  generated: /data/octoprint/generated" >> config.yaml
-        echo "  logs: /data/octoprint/logs" >> config.yaml
-        echo "  plugins: /data/octoprint/plugins" >> config.yaml
-        echo "  virtualSd: /data/octoprint/virtualSd" >> config.yaml
-        # User-accessible folders
-        echo "  printerProfiles: /config/octoprint/printerProfiles" >> config.yaml
-        echo "  slicingProfiles: /config/octoprint/slicingProfiles" >> config.yaml
-        echo "  scripts: /config/octoprint/scripts" >> config.yaml
-        echo "  timelapse: /config/octoprint/timelapse" >> config.yaml
-        echo "  timelapse_tmp: /config/octoprint/timelapse/tmp" >> config.yaml
-        echo "  translations: /config/octoprint/translations" >> config.yaml
-        echo "  uploads: /config/octoprint/uploads" >> config.yaml
-        echo "  watched: /config/octoprint/watched" >> config.yaml
-        echo "plugins:" >> config.yaml
-        echo "  cura:" >> config.yaml
-        echo "    cura_engine: /sbin/CuraEngine" >> config.yaml
-        echo "    debug_logging: false" >> config.yaml
-        echo "server:" >> config.yaml
-        echo "  commands:" >> config.yaml
-        echo "    serverRestartCommand: supervisorctl restart octoprint" >> config.yaml
-        echo "    systemRestartCommand: /restart.sh" >> config.yaml
-        echo "    systemShutdownCommand: /shutdown.sh" >> config.yaml
-        echo "webcam:" >> config.yaml
-        echo "  ffmpeg: /usr/bin/ffmpeg" >> config.yaml
-        # echo "  stream: /webcam/?action=stream" >> config.yaml
-        # echo "  snapshot: http://127.0.0.1:8080/?action=snapshot" >> config.yaml
-        # echo "serial:" >> config.yaml
-        # echo "  additionalPorts:" >> config.yaml
-        # echo "  - /dev/tty*" >> config.yaml
-        # echo "    systemRestartCommand: bashio::addon.restart" >> config.yaml
-        # echo "    systemShutdownCommand: bashio::addon.stop" >> config.yaml
-        # echo "system:" >> config.yaml
-        # echo "  actions:" >> config.yaml
-        # echo "  - action: streamon" >> config.yaml
-        # echo "    command: supervisorctl start mjpg-streamer" >> config.yaml
-        # echo "    confirm: false" >> config.yaml
-        # echo "    name: Start webcam" >> config.yaml
-        # echo "  - action: streamoff" >> config.yaml
-        # echo "    command: supervisorctl stop mjpg-streamer" >> config.yaml
-        # echo "    confirm: false" >> config.yaml
-        # echo "    name: Stop webcam" >> config.yaml
-        
+        cp /root/config_octoprint.yaml /config/octoprint/config.yaml
     fi
 }
 
