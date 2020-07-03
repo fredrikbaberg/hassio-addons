c4c4286c8943099844cf820b618c30cdf59523c3
Fredrik Baberg <fredrik.baberg@gmail.com>
2020-07-03 11:27:31 +0200

    Attempt to solve mjpg plugin not found

--- octoprint/FS/mjpgstreamer.sh
+++ octoprint/FS/mjpgstreamer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/with-contenv bashio
-
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 exec mjpg_streamer -i \"$(bashio::config 'mjpg_input')\" -o \"=$(bashio::config 'mjpg_output')\"
