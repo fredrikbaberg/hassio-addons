ec79ed9b667cfcb932d7f3d1f5713b4de48dfa7e
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-01-23 00:22:39 +0100

    Cleanup logs, remove mjpgstreamer from autostart

--- octoprint_dev/build_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Build a Docker image and test to launch it (replacing the entrypoint with /bin/sh). For testing.
-mkdir -p my_test_data && mkdir -p my_test_config
-sudo docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/octoprint_test .
-sudo docker run -it --rm -v $(pwd)/my_test_data:/data -v $(pwd)/my_test_config:/config -p 5000:80 --device=/dev/video0:/dev/video0 --entrypoint /bin/sh local/octoprint_test
-# docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -t octoprint -r https://github.com/fredrikbaberg/hassio-addons -b build_update
