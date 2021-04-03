08c15fec65b1084ee72f1f4a1f83479981013c6b
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-04-03 01:25:23 +0000

    Basic Caddy working, except camera in Ingress

--- octoprint_dev/rootfs/etc/cont-init.d/caddy.sh
+++ octoprint_dev/rootfs/etc/cont-init.d/caddy.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/with-contenv bashio
 
 bashio::log.info "Caddy cont-init.d"
+
+if [ ! -f /config/Caddyfile ]; then
+    cp /etc/services.d/caddy/Caddyfile /config/Caddyfile
+fi
+
+ingress_entry=$(bashio::addon.ingress_entry)
+sed -i "s#%%base_path%%#${ingress_entry}#g" /config/Caddyfile
+# sed -i "s#%%base_path%%#${ingress_entry}#g" /etc/services.d/caddy/Caddyfile
