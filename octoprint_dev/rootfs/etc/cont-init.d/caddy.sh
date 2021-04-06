9720fd7cb250829c273e6e87a78e577f0163cbe6
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-04-06 17:42:18 +0200

    Cleanup and update mostly Caddy

--- octoprint_dev/rootfs/etc/cont-init.d/caddy.sh
+++ octoprint_dev/rootfs/etc/cont-init.d/caddy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
 bashio::log.info "Caddy cont-init.d"
+ingress_entry=$(bashio::addon.ingress_entry)
 
 if [ -f "/config/octoprint"$(bashio::config 'config_folder_suffix')/Caddyfile ]; then
-    ingress_entry=$(bashio::addon.ingress_entry)
     sed -i "s#%%base_path%%#${ingress_entry}#g" "/config/octoprint"$(bashio::config 'config_folder_suffix')/Caddyfile
 fi
--- octoprint_dev/rootfs/etc/cont-init.d/tinyproxy.sh
+++ octoprint_dev/rootfs/etc/cont-init.d/tinyproxy.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/with-contenv bashio
 
 bashio::log.info "TinyProxy cont-init.d"
+ingress_entry=$(bashio::addon.ingress_entry)
 
 if [ -f "/config/octoprint"$(bashio::config 'config_folder_suffix')/tinyproxy-ingress.conf ]; then
-    ingress_entry=$(bashio::addon.ingress_entry)
     sed -i "s#%%base_path%%#${ingress_entry}#g" "/config/octoprint"$(bashio::config 'config_folder_suffix')/tinyproxy-ingress.conf
 fi
 if [ -f "/config/octoprint"$(bashio::config 'config_folder_suffix')/tinyproxy-webui.conf ]; then
-    ingress_entry=$(bashio::addon.ingress_entry)
     sed -i "s#%%base_path%%#${ingress_entry}#g" "/config/octoprint"$(bashio::config 'config_folder_suffix')/tinyproxy-webui.conf
 fi
