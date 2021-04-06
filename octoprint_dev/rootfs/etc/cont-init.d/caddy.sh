89b6ecd30ac38f6eed6c37bbe83e59d966801965
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-04-06 17:58:31 +0200

    Cleanup of proxies, only use Caddy.

--- octoprint_dev/rootfs/etc/cont-init.d/caddy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-bashio::log.info "Caddy cont-init.d"
-ingress_entry=$(bashio::addon.ingress_entry)
-
-if [ -f "/config/octoprint"$(bashio::config 'config_folder_suffix')/Caddyfile ]; then
-    sed -i "s#%%base_path%%#${ingress_entry}#g" "/config/octoprint"$(bashio::config 'config_folder_suffix')/Caddyfile
-fi
--- octoprint_dev/rootfs/etc/cont-init.d/haproxy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-bashio::log.info "HAProxy cont-init.d"
-ingress_entry=$(bashio::addon.ingress_entry)
-
-if [ -f "/config/octoprint"$(bashio::config 'config_folder_suffix')/haproxy.cfg ]; then
-    sed -i "s#%%base_path%%#${ingress_entry}#g" "/config/octoprint"$(bashio::config 'config_folder_suffix')/haproxy.cfg
-fi
