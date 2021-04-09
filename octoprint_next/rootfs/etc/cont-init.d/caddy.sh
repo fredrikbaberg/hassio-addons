7a70cd79edd3c40797738e81b7b00406eaa3b984
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-04-09 23:47:27 +0200

    Update Caddy for recovery

--- octoprint_next/rootfs/etc/cont-init.d/caddy.sh
+++ octoprint_next/rootfs/etc/cont-init.d/caddy.sh
@@ -7,5 +7,5 @@ ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s#%%base_path%%#${ingress_entry}#g" /etc/caddy/Caddyfile
 
 if bashio::config.true 'recovery'; then
-    sed -i "s\# insert_rescue\rewrite / /recovery\g" /etc/caddy/Caddyfile
+    sed -i "s%# insert_rescue%rewrite / /recovery%g" /etc/caddy/Caddyfile
 fi
