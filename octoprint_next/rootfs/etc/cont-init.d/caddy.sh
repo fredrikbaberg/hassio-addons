da2b128c562451b9eda9586e6f7525808ca2c1fa
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-04-09 23:19:34 +0200

    Add option for recovery

--- octoprint_next/rootfs/etc/cont-init.d/caddy.sh
+++ octoprint_next/rootfs/etc/cont-init.d/caddy.sh
@@ -5,3 +5,7 @@ bashio::log.info "Caddy cont-init.d"
 ingress_entry=$(bashio::addon.ingress_entry)
 
 sed -i "s#%%base_path%%#${ingress_entry}#g" /etc/caddy/Caddyfile
+
+if bashio::config.true 'recovery'; then
+    sed -i "s#@ingress http://127.0.0.1:80#@ingress http://127.0.0.1:80/recovery#g" /etc/caddy/Caddyfile
+fi
