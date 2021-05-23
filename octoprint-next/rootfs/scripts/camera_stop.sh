ba47661dad9f99f5aba2462e44803b14d5ed7f7f
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-05-24 01:23:01 +0200

    OctoPrint Next attempt to fix camera stop issue (#156)
    * Attempt to fix #155 through other s6-svc command and log entry.
    * Correction to Shields version

--- octoprint-next/rootfs/scripts/camera_stop.sh
+++ octoprint-next/rootfs/scripts/camera_stop.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
-s6-svc -wD -d /var/run/s6/services/mjpg-streamer
+bashio::log.info "Stop camera"
+
+s6-svc -t /var/run/s6/services/mjpg-streamer
 
 # supervisorctl stop mjpg-streamer
