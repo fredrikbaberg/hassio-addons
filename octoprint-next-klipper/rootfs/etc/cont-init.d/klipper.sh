4fa623606656b693d303f623c4c0d4a7ff292532
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-06-08 02:06:14 +0200

    Simplified Klipper

--- octoprint-next-klipper/rootfs/etc/cont-init.d/klipper.sh
+++ octoprint-next-klipper/rootfs/etc/cont-init.d/klipper.sh
@@ -2,12 +2,6 @@
 
 bashio::log.info "Prepare Klipper"
 
-if [ ! -d /data/klippy-env ]; then
-    tar -zxf /root/klippy-env.tar.gz -C /data/
-    rm -rf /root/klippy-env.tar.gz
-fi
+mkdir -p /data/klipper/config
 
-if [ ! -d /data/klipper ]; then
-    tar -zxf /root/klipper.tar.gz -C /data/
-    rm -rf /root/klipper.tar.gz
-fi
+touch /data/klipper/config/printer.cfg
