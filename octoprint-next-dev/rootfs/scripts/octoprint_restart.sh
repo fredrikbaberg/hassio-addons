8b9410b8350771a049cc66037d94852268b260d5
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-03 16:31:29 +0200

    Add delay to start OctoPrint

--- octoprint-next-dev/rootfs/scripts/octoprint_restart.sh
+++ octoprint-next-dev/rootfs/scripts/octoprint_restart.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.info "Attempt to restart OctoPrint"
+bashio::log.warning "Attempt to restart OctoPrint."
 
-s6-svc -wD -d -T2500 /var/run/s6/services/octoprint
+/bin/s6-svc -wD -d /var/run/s6/services/octoprint
 sleep 5
-bashio::log.info "Attempt to restart OctoPrint"
-# s6-svc -wU -u -T2500 /var/run/s6/services/octoprint
-
-# /bin/s6-svc -r /var/run/s6/services/octoprint
+bashio::log.warning "Should have restarted now."
 # /bin/s6-svc -wU -u /var/run/s6/services/octoprint
 # /bin/s6-svc -wd -d /var/run/s6/services/octoprint
+# /bin/s6-svc -r /var/run/s6/services/octoprint
