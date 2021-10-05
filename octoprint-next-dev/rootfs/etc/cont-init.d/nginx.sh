5e5459bbaa64ed806576d34574ab8936993859a4
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-06 01:01:21 +0200

    Updates for nginx

--- octoprint-next-dev/rootfs/etc/cont-init.d/nginx.sh
+++ octoprint-next-dev/rootfs/etc/cont-init.d/nginx.sh
@@ -5,9 +5,6 @@ bashio::log.info "nginx cont-init.d"
 ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
 
-mkdir -p /var/log/nginx
-touch /var/log/nginx/error.log
-
 # sed -i "s#%%base_path%%#$(bashio::addon.ingress_entry)#g" /etc/nginx/http.d/ingress.conf
 # if bashio::config.true 'recovery'; then
 #     sed -i "s%#insert_rescue%rewrite / /recovery%g" /etc/caddy/Caddyfile
