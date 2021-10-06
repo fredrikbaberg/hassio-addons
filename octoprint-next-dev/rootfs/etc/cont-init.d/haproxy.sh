b84d89470cd944e9a2d5bd23f82bd99f1118ba00
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-07 01:07:48 +0200

    Exclude Klipper src in backup; clean haproxy prep

--- octoprint-next-dev/rootfs/etc/cont-init.d/haproxy.sh
+++ octoprint-next-dev/rootfs/etc/cont-init.d/haproxy.sh
@@ -2,7 +2,4 @@
 
 bashio::log.info "HAProxy cont-init.d"
 
-ingress_entry=$(bashio::addon.ingress_entry)
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.cfg
-
-bashio::log.info "Ingress entry: ${ingress_entry}"
+sed -i "s#%%ingress_entry%%#${bashio::addon.ingress_entry}#g" /etc/haproxy/haproxy.cfg
