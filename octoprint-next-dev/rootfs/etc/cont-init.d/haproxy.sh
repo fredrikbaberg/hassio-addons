99dbec2f83c2baaaa18582ae91b6ba5cb7e63be2
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-06 17:17:31 +0200

    Change to HAProxy

---
+++ octoprint-next-dev/rootfs/etc/cont-init.d/haproxy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "HAProxy cont-init.d"
+
+ingress_entry=$(bashio::addon.ingress_entry)
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.cfg
