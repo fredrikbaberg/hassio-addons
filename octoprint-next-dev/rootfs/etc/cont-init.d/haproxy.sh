5a4f39c77a23865f2afc66eed318b82272e2c87e
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-06 22:12:20 +0000

    Update HAProxy path

--- octoprint-next-dev/rootfs/etc/cont-init.d/haproxy.sh
+++ octoprint-next-dev/rootfs/etc/cont-init.d/haproxy.sh
@@ -4,3 +4,5 @@ bashio::log.info "HAProxy cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.cfg
+
+bashio::log.info "Ingress entry: ${ingress_entry}"
