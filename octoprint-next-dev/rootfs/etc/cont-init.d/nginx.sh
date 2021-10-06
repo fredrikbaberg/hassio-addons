e7e0e5f937164e7304709925db4e4fd079586381
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-06 15:00:36 +0200

    Octoprint next dev (#184)
    * Add missing dependency
    
    * Update version number
    
    * Updates for nginx
    
    * Update nginx config
    
    * Update version
    
    * Update config syntax
    
    * Update config syntax
    
    * Update mjpg-streamer script
    
    * Update version
    
    * Change owner of nginx files
    
    * Disable nginx log
    
    * Remove nginx log folder
    
    * Build image
    
    * More updates to nginx
    
    * Build for arm, add SuperSlicer again
    
    * Update for nginx, again
    
    * Add libffi-dev, required for Klipper in ARM
    
    * Disable SuperSlicer, again
    
    * Update version number
    
    * Build locally
    
    * Add build-essential
    
    * Remove SED command
    
    * Change user of haproxy
    
    * Build with most components

--- octoprint-next-dev/rootfs/etc/cont-init.d/nginx.sh
+++ octoprint-next-dev/rootfs/etc/cont-init.d/nginx.sh
@@ -4,7 +4,6 @@ bashio::log.info "nginx cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/http.d/ingress.conf
 
 # sed -i "s#%%base_path%%#$(bashio::addon.ingress_entry)#g" /etc/nginx/http.d/ingress.conf
 # if bashio::config.true 'recovery'; then
