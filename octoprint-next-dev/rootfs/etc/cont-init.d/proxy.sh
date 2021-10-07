ac4020e1432426b74e532fe4f4a4368d04b5ae30
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-07 03:04:16 +0200

    Use TinyProxy as reverse proxy

---
+++ octoprint-next-dev/rootfs/etc/cont-init.d/proxy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "proxy cont-init.d"
+
+ingress_entry=$(bashio::addon.ingress_entry)
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/tinyproxy/tinyproxy-ingress.conf
+
+# sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
