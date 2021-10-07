2f2014d01a2d47679b55e0741880b076519d00ee
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-07 10:29:47 +0000

    Revert from TinyProxy to HAProxy

--- octoprint-next-dev/rootfs/etc/cont-init.d/proxy.sh
+++ octoprint-next-dev/rootfs/etc/cont-init.d/proxy.sh
@@ -3,6 +3,6 @@
 bashio::log.info "proxy cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/tinyproxy/tinyproxy-ingress.conf
-
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.conf
+# sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/tinyproxy/tinyproxy-ingress.conf
 # sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
