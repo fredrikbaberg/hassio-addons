0404c743ff8711a4cf5c57866093acd05ab573b9
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-11 12:18:56 +0200

    Update mjpg-streamer run

--- octoprint-next-dev/rootfs/etc/cont-init.d/mjpg-streamer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-bashio::log.info "mjpg-streamer cont-init.d"
-
-mjpg_arguments=$(bashio::config 'mjpg_arguments')
-
-sed -i "s#%%mjpg_arguments%%#$(bashio::config 'mjpg_arguments')#g" /var/run/s6/services/mjpg-streamer/run
