7e7954efcb6f1711ee4a24da62a410793bb000b2
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-11 13:15:28 +0200

    Use single line for mjpg args

--- octoprint-next-dev/rootfs/etc/cont-init.d/proxy.sh
+++ octoprint-next-dev/rootfs/etc/cont-init.d/proxy.sh
@@ -4,9 +4,6 @@ bashio::log.info "proxy cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s#%%ingress_entry%%#$(bashio::addon.ingress_entry)#g" /etc/caddy/Caddyfile
-# sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.cfg
-# sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/tinyproxy/tinyproxy-ingress.conf
-# sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
 
 if bashio::config.true 'recovery'; then
     sed -i "s%#insert_rescue%rewrite / /recovery%g" /etc/caddy/Caddyfile
