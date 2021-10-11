213c564263999c915da26fcee1cbe94864ebda16
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-11 14:10:53 +0200

    Update mjpg-streamer args before copy

--- octoprint-next-dev/rootfs/etc/cont-init.d/mjpg-streamer.sh
+++ octoprint-next-dev/rootfs/etc/cont-init.d/mjpg-streamer.sh
@@ -3,4 +3,4 @@
 bashio::log.info "mjpg-streamer cont-init.d"
 
 mjpg_arguments=$(bashio::addon.ingress_entry)
-sed -i "s#%%mjpg_arguments%%#$mjpg_arguments#g" /var/run/s6/services/mjpg-streamer/run
+sed -i "s#%%mjpg_arguments%%#$mjpg_arguments#g" /etc/services.d/mjpg-streamer/run
