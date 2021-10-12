7b2e32bf9d235ae0571e3f4815cfff7ac5715483
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-12 12:09:26 +0000

    Use pre-built instead of local

--- octoprint-next-dev/rootfs/etc/cont-init.d/mjpg-streamer.sh
+++ octoprint-next-dev/rootfs/etc/cont-init.d/mjpg-streamer.sh
@@ -2,5 +2,10 @@
 
 bashio::log.info "mjpg-streamer cont-init.d"
 
-mjpg_arguments=$(bashio::config 'mjpg_arguments')
-sed -i "s#%%mjpg_arguments%%#$mjpg_arguments#g" /etc/services.d/mjpg-streamer/run
+MJPG_ARGUMENTS=""
+for I in $(bashio::config 'mjpg_arguments'); do
+    to_add=${I//$'\n'/}
+    MJPG_ARGUMENTS+="$to_add "
+done
+
+echo "exec mjpg_streamer $MJPG_ARGUMENTS" >> /etc/services.d/mjpg-streamer/run
