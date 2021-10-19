56466c6d7a22c3581d1933e5fd52801e0440c14b
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-19 23:50:29 +0200

    Correction to path

--- octoprint-base/rootfs/etc/cont-init.d/proxy.sh
+++ octoprint-base/rootfs/etc/cont-init.d/proxy.sh
@@ -4,7 +4,7 @@ bashio::log.info "Proxy cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
 
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.cfg
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/caddy/Caddyfile
 
 # if bashio::config.true 'recovery'; then
 #     sed -i "s%#insert_rescue%rewrite / /recovery%g" /etc/caddy/Caddyfile
