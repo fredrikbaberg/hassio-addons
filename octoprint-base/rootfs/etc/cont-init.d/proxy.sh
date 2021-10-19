2a8ef9891887ec929f6dc0a4992cc64b10888d02
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-19 02:55:12 +0200

    Cleanup as preparation for extended (#206)

---
+++ octoprint-base/rootfs/etc/cont-init.d/proxy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "Proxy cont-init.d"
+
+ingress_entry=$(bashio::addon.ingress_entry)
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.cfg
+
+# if bashio::config.true 'recovery'; then
+#     sed -i "s%#insert_rescue%rewrite / /recovery%g" /etc/caddy/Caddyfile
+# fi
+
+
