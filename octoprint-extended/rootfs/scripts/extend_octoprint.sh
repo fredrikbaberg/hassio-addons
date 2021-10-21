81dd5a48ca7d295e547927575b6dd85d65c5e098
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-21 17:32:25 +0200

    Update additional packages, add script to install extras

--- octoprint-extended/rootfs/scripts/extend_octoprint.sh
+++ octoprint-extended/rootfs/scripts/extend_octoprint.sh
@@ -4,4 +4,6 @@ echo "Install additional packages for OctoPrint, e.g. for marlin-binary-protocol
 
 source /data/python/OctoPrint/bin/activate
 pip install ${PIP_FLAGS} cython
-pip install ${PIP_FLAGS} marlin-binary-protocol
+pip install ${PIP_FLAGS} \
+    marlin-binary-protocol \
+    psycopg2
