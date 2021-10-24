eee550857523ae951b21126290d0a55fefc716ba
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-24 02:33:55 +0200

    Update haproxy

--- octoprint-extended/rootfs/etc/cont-init.d/proxy.sh
+++ octoprint-extended/rootfs/etc/cont-init.d/proxy.sh
@@ -4,7 +4,7 @@ bashio::log.info "Proxy cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
 
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /data/proxy/haproxy.cfg
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.cfg
 
 # if bashio::config.true 'recovery'; then
 #     sed -i "s%#recovery%rewrite / /recovery%g" /etc/caddy/Caddyfile
