77314065e475aad94b0efdf6dc3d4be51a1dc8b1
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-24 03:38:18 +0200

    Revert to Caddy

--- octoprint-extended/rootfs/etc/cont-init.d/proxy.sh
+++ octoprint-extended/rootfs/etc/cont-init.d/proxy.sh
@@ -4,8 +4,8 @@ bashio::log.info "Proxy cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
 
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/haproxy/haproxy.cfg
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/caddy/Caddyfile
 
-# if bashio::config.true 'recovery'; then
-#     sed -i "s%#recovery%rewrite / /recovery%g" /etc/caddy/Caddyfile
-# fi
+if bashio::config.true 'recovery'; then
+    sed -i "s%#recovery%rewrite / /recovery%g" /etc/caddy/Caddyfile
+fi
