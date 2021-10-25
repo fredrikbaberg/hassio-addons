0311d77a0c170ebd87718ed43516aa7f636c6eb3
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-25 14:00:51 +0200

    Remove octoprint-base

--- octoprint-base/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# Changelog
-
-[0.1.1]
-
-Add ffmpeg
-
-[0.1.0]
-
-Initial version of base image
--- octoprint-base/rootfs/scripts/octoprint_restart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-bashio::log.warning "Restart OctoPrint."
-
-s6-svc -r /var/run/s6/services/octoprint
