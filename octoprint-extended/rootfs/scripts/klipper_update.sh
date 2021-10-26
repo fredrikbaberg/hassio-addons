85da1c514b7385e4e810956609bba51dcd7c5204
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-26 11:40:42 +0200

    Don't compile in Klipper update script

--- octoprint-extended/rootfs/scripts/klipper_update.sh
+++ octoprint-extended/rootfs/scripts/klipper_update.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Update Klipper to latest version (git master branch)"
+echo "Update Klipper source to latest version (git master branch)"
 
 cd /data/klipper
 # Stash changes
 git stash
 # Update from repository
-git pull origin master
-# Apply fix for scheduler
-sed -i 's"// sched_main"// sched_main\n#include <pthread.h>"' /data/klipper/src/linux/main.c
-sed -i 's/sched_setscheduler(0/pthread_setschedparam(pthread_self()/' /data/klipper/src/linux/main.c
-# Make, install and clean
-make clean
-make
-./scripts/flash-linux.sh
-make clean
+git checkout master
+git pull
+# git pull origin master
+# # Apply fix for scheduler
+# sed -i 's"// sched_main"// sched_main\n#include <pthread.h>"' /data/klipper/src/linux/main.c
+# sed -i 's/sched_setscheduler(0/pthread_setschedparam(pthread_self()/' /data/klipper/src/linux/main.c
+# # Make, install and clean
+# make clean
+# make
+# ./scripts/flash-linux.sh
+# make clean
