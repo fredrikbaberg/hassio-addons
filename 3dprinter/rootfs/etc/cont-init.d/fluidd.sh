e69dd79c2937b6b9e8946fdc0cce7713c005f519
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-29 03:32:37 +0200

    Attempt to fix proxy for Fluidd

--- 3dprinter/rootfs/etc/cont-init.d/fluidd.sh
+++ 3dprinter/rootfs/etc/cont-init.d/fluidd.sh
@@ -5,8 +5,8 @@ bashio::log.info "Fluidd cont-init.d"
 # Copy Klipper install to persistent storage, if missing.
 if [ ! -d /data/fluidd ]; then
     if [ -f /root/Fluidd.tar.gz ]; then
-        mkdir -p /data/fluidd
-        tar -zxf /root/Fluidd.tar.gz -C /data/fluidd
+        mkdir -p /data
+        tar -zxf /root/Fluidd.tar.gz -C /data
         bashio::log.notice "Fluidd extracted"
     else
         bashio::log.warning "Fluidd archive not found"
