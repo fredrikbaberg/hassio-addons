e668791859892b53673751d0b38870de1f93b009
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-10-29 13:38:59 +0200

    Some changes to Caddy

--- 3dprinter/rootfs/etc/cont-init.d/proxy.sh
+++ 3dprinter/rootfs/etc/cont-init.d/proxy.sh
@@ -2,15 +2,7 @@
 
 bashio::log.info "Proxy cont-init.d"
 
-ingress_entry=$(bashio::addon.ingress_entry)
-
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/caddy/Caddyfile
-
-if bashio::config.true 'recovery'; then
-    sed -i "s%#recovery%rewrite / /recovery%g" /etc/caddy/Caddyfile
-fi
-
-export ingress_entry=$ingress_entry
+export ingress_entry=$(bashio::addon.ingress_entry)
 tempio \
     -conf /data/options.json \
     -template /usr/share/tempio/Caddyfile \
