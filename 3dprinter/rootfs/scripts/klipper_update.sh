fcda60b4c1816c1a680b716ee7b8848aae001af6
Fredrik Baberg <fredrik.baberg@gmail.com>
2021-11-20 00:48:03 +0100

    Add error check for klipper_mcu in klipper update

--- 3dprinter/rootfs/scripts/klipper_update.sh
+++ 3dprinter/rootfs/scripts/klipper_update.sh
@@ -15,12 +15,16 @@ pip install ${PIP_FLAGS} -r /data/src/klipper/scripts/klippy-requirements.txt
 deactivate
 
 # Update klipper_mcu
-make clean
-# # Apply fix for scheduler
-# sed -i 's"// sched_main"// sched_main\n#include <pthread.h>"' /data/klipper/src/linux/main.c
-# sed -i 's/sched_setscheduler(0/pthread_setschedparam(pthread_self()/' /data/klipper/src/linux/main.c
-make
-mkdir -p /data/bin
-rm -rf /data/bin/klipper_mcu
-cp out/klipper.elf /data/bin/klipper_mcu
-make clean
+{
+    make clean
+    # # Apply fix for scheduler
+    # sed -i 's"// sched_main"// sched_main\n#include <pthread.h>"' /data/klipper/src/linux/main.c
+    # sed -i 's/sched_setscheduler(0/pthread_setschedparam(pthread_self()/' /data/klipper/src/linux/main.c
+    make
+    mkdir -p /data/bin
+    rm -rf /data/bin/klipper_mcu
+    cp out/klipper.elf /data/bin/klipper_mcu
+    make clean
+} || {
+    bashio::log.warning "Failed to update klipper_mcu"
+}
