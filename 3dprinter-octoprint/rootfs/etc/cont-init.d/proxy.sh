872a111c96ac048244022c124fcaac7b457b40c7
Fredrik Baberg <fredrik.baberg@gmail.com>
2022-05-08 10:05:46 +0200

    3dprinter-octoprint ready for testing

--- 3dprinter-octoprint/rootfs/etc/cont-init.d/proxy.sh
+++ 3dprinter-octoprint/rootfs/etc/cont-init.d/proxy.sh
@@ -4,6 +4,9 @@ bashio::log.info "Proxy cont-init.d"
 
 ingress_entry=$(bashio::addon.ingress_entry)
 export ingress_entry=${ingress_entry}
+
+ls -lah /etc/caddy
+
 tempio \
     -conf /data/options.json \
     -template /usr/share/tempio/Caddyfile \
