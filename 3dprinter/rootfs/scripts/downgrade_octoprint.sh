de6cb02b71cc725d5aa9b43d697ba207aab137b6
Fredrik Baberg <fredrik.baberg@gmail.com>
2023-01-13 14:41:06 +0100

    Initial attempt at downgrade mode

---
+++ 3dprinter/rootfs/scripts/downgrade_octoprint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.warning "Check if OctoPrint version should be downgraded"
+
+{
+    version="$(octoprint -b /data/config/octoprint --version)"
+    # Check if version is available.
+    if [[ ! -z $version ]]; then
+        if [[ $version == *"1.7"* ]]; then
+            echo "Version is OK"
+        else
+            echo "Need to change version."
+            pip install --upgrade OctoPrint==1.7.3
+        fi
+    else
+        echo "Version not available, won't do anything."
+    fi
+} || {
+    echo "Unable to downgrade OctoPrint version."
+}
